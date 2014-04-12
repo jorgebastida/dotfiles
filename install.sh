16dcf464be858389f8c1eca39c6e78a2782b3821
Jorge Bastida <me@jorgebastida.com>
2014-04-12 18:23:54 +0100

    Add virtualenvwrapper postactivate hook

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ ln -s $PWD/gitconfig ~/.gitconfig
 
 mv ~/.bash_profile ~/.bash_profile.backup || true
 ln -s $PWD/bash_profile ~/.bash_profile
+
+mv ~/.virtualenvs/postactivate ~/.virtualenvs/postactivate.backup || true
+ln -s $PWD/virtualenvwrapper_postactivate ~/.virtualenvs/postactivate
+
